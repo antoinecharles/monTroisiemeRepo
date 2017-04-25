2c03565e67059b77fd7f777ab94a10f36dacc7be
Tapouere <antoine.charles2@gmail.com>
2017-04-25 22:59:41 +0200

    Ajout bouton reset

--- something.js
+++ something.js
@@ -4,5 +4,12 @@ function iterate(){
 	console.log(counter);
 	counter = counter + 1;
 	console.log(counter);
+	updateCount();
+}
+function reset(){
+	counter = 0;
+	updateCount();
+}
+function updateCount(){
 	document.getElementById("count").innerHTML = counter;
 }
